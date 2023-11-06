985be9de2ef346e7012957fe3e80fd22cbc7635e
amxsupport <itmasterdesigne@gmail.com>
2023-11-06 20:25:23 +0000

    update base_model.py add def to_dict and --str--

--- models/base_model.py
+++ models/base_model.py
@@ -33,4 +33,20 @@ class BaseModel:
         self.updated_at = datetime.today()
         models.storage.save()
 
+    def to_dict(self):
+        """Return the dictionary of the BaseModel instance.
+
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
 
