d4d01c0679de28c7e5435d2f4acef7586674fc91
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2023-11-07 14:41:34 +0100

    Update console.py start implementing do_update function

--- console.py
+++ console.py
@@ -102,6 +102,14 @@ class HBNBCommand(cmd.Cmd):
                 count += 1
         print(count)
 
+    def do_update(self, arg):
+        """Usage: update <class> <id> <attribute_name> <attribute_value> or
+       <class>.update(<id>, <attribute_name>, <attribute_value>) or
+       <class>.update(<id>, <dictionary>)
+        Update a class instance of a given id by adding or updating
+        a given attribute key/value pair or dictionary."""
+        argl = parse(arg)
+        objdict = storage.all()
 
 
 if __name__ == "__main__":
