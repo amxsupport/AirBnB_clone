06758d2d78c336bcadb6e2cabf4d150c639ebb2c
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2023-11-07 11:47:28 +0100

    Create file_storage.py

---
+++ models/engine/file_storage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines the FileStorage class."""
+import json
+from models.base_model import BaseModel
+
+
+
+class FileStorage:
+    """Represent an abstracted storage engine.
+
+    Attributes:
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
+    """
+    __file_path = "file.json"
+    __objects = {}
