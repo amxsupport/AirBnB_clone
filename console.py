305cb4a610901c401b38993ba1c3d832e38a101e
amxsupport <itmasterdesigne@gmail.com>
2023-11-08 12:47:18 +0000

    update console.py add parse function

--- console.py
+++ console.py
@@ -5,7 +5,25 @@ import re
 from shlex import split
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
 
+
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
 class HBNBCommand(cmd.Cmd):
     """Defines the HBnB command interpreter.
@@ -14,6 +32,11 @@ class HBNBCommand(cmd.Cmd):
         prompt (str): The command prompt.
     """
 
+prompt = "(hbnb) "
+    __classes = {
+        "BaseModel",
+        "User"
+    }
 
     def emptyline(self):
         """Do nothing upon receiving an empty line."""
