23b663a78a0717c5cc4c3c4aa92e870761f3b09a
amxsupport <itmasterdesigne@gmail.com>
2023-11-12 21:17:17 +0000

    create test_review.py

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines unittests for models/review.py.
+
+Unittest classes:
+    TestReview_instantiation
+    TestReview_save
+    TestReview_to_dict
+"""
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.review import Review
+
+
+class TestReview_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the Review class."""
+
+    def test_no_args_instantiates(self):
+        self.assertEqual(Review, type(Review()))
+
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(Review(), models.storage.all().values())
+
+
