7b41deeb802de012a90b7875b5ebe61b637a8362
amxsupport <itmasterdesigne@gmail.com>
2023-11-12 21:30:35 +0000

    update test_review.py update TestReview_to_dict

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -165,5 +165,33 @@ class TestReview_to_dict(unittest.TestCase):
         self.assertEqual("Holberton", rv.middle_name)
         self.assertIn("my_number", rv.to_dict())
 
+    def test_to_dict_datetime_attributes_are_strs(self):
+        rv = Review()
+        rv_dict = rv.to_dict()
+        self.assertEqual(str, type(rv_dict["id"]))
+        self.assertEqual(str, type(rv_dict["created_at"]))
+        self.assertEqual(str, type(rv_dict["updated_at"]))
+
+    def test_to_dict_output(self):
+        dt = datetime.today()
+        rv = Review()
+        rv.id = "123456"
+        rv.created_at = rv.updated_at = dt
+        tdict = {
+            'id': '123456',
+            '__class__': 'Review',
+            'created_at': dt.isoformat(),
+            'updated_at': dt.isoformat(),
+        }
+        self.assertDictEqual(rv.to_dict(), tdict)
+
+    def test_contrast_to_dict_dunder_dict(self):
+        rv = Review()
+        self.assertNotEqual(rv.to_dict(), rv.__dict__)
+
+    def test_to_dict_with_arg(self):
+        rv = Review()
+        with self.assertRaises(TypeError):
+            rv.to_dict(None)
 
 
