0e29eacb3eb4bff371f197757be08b364f66ded4
amxsupport <itmasterdesigne@gmail.com>
2023-11-12 21:27:33 +0000

    update test_review.py create TestReview_save

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -95,4 +95,24 @@ class TestReview_instantiation(unittest.TestCase):
         with self.assertRaises(TypeError):
             Review(id=None, created_at=None, updated_at=None)
 
+class TestReview_save(unittest.TestCase):
+    """Unittests for testing save method of the Review class."""
+
+    @classmethod
+    def setUp(self):
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+
 
